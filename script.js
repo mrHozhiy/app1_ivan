8ba9fa8d21cd5645fb468c116cf1b74f434f5a23
Dmitriy Hozhiy <gun.dimonchik@gmail.com>
2021-11-16 20:41:11 +0200

    fix part_2

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function start() {
     }
 }
 
-start();
+// start();
 
 
 const personalMovieDB = {
@@ -34,29 +34,33 @@ const personalMovieDB = {
     privat: false
 };
 
+function rememberMyFilms() {
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов?", ""),
-        b = prompt("На сколько оцените его?", "");
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов?", ""),
+            b = prompt("На сколько оцените его?", "");
 
-
-    if (a != null || b != null || a != '' || b != '' || a.length > 50) {
-
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
 
+rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else {
-    console.log("Вы киноман");
+
+function detectionPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else {
+        console.log("Вы киноман");
+    }
 }
 
 
